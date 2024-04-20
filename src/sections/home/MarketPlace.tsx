ef3792296987e6151caa991a86a456064dae8ec6
DamifeZion <damifemartins@gmail.com>
2024-04-20 08:34:26 +0100

    completed marketplace responsiveness

--- src/sections/home/MarketPlace.tsx
+++ src/sections/home/MarketPlace.tsx
@@ -5,14 +5,14 @@ const MarketPlace = () => {
    const { itDevelopment, design } = homeConstants.marketPlace;
 
    return (
-      <div className="grid gap-7">
-         <div className="flex justify-between">
-            <div id="text-cont" className="py-6 max-w-[350px]">
-               <h1 className="text-2xl font-medium">
+      <div className="flex flex-col gap-7">
+         <div className="flex flex-col justify-betwee box-border lg:grid lg:grid-cols-2">
+            <div id="text-cont" className="py-6 max-w-[350px] max-lg:mx-auto">
+               <h1 className="text-2xl font-medium text-center lg:text-start">
                   Find Dev and IT professionals to scale your business.
                </h1>
 
-               <div className="pt-5 grid grid-cols-2 items-center space-x-2 *:flex *:items-center *:gap-2">
+               <div className="pt-5 flex justify-between items-center space-x-2 md:grid md:grid-cols-2 *:flex *:items-center *:gap-2 *:text-sm md:text:md">
                   <span>
                      <img loading="lazy" src="/home/medal.svg" />
                      <p>989 Skills</p>
@@ -28,13 +28,15 @@ const MarketPlace = () => {
             {/*  */}
             <Card
                id="card"
-               className="max-w-[740px] py-[18px] px-7 rounded-sm shadow-none"
+               className="max-w-[740px] w-full py-[18px] px-4 rounded-sm shadow-none sm:mx-auto lg:px-7"
             >
                <CardHeader className="px-0 py-0">
-                  <CardTitle className="font-medium text-md">
+                  <CardTitle className="font-medium text-sm md:text-md">
                      IT & Development
-                  </CardTitle>
+                  </CardTitle>                  
+               </CardHeader>
 
+               <div className="overflow-x-auto pr-0">
                   <CardContent className="pt-2 px-0 pb-0 flex gap-4">
                      {itDevelopment.map((data, index) => (
                         <div
@@ -65,18 +67,21 @@ const MarketPlace = () => {
                         </span>
                      </div>
                   </CardContent>
-               </CardHeader>
+               </div>
             </Card>
          </div>
 
+
+
+
          {/*=== Section TWO ===*/}
-         <div className="flex justify-between">
-            <div id="text-cont" className="py-6 max-w-[350px]">
-               <h1 className="text-2xl font-medium">
+         <div className="flex flex-col justify-between box-border  lg:grid lg:grid-cols-2">
+            <div id="text-cont" className="py-6 lg:max-w-[350px] max-lg:mx-auto">
+               <h1 className="max-w-[350px] mx-auto text-2xl font-medium text-center lg:text-start">
                   Explore Creative individuals with a keen eye for detail.
                </h1>
 
-               <div className="pt-5 grid grid-cols-2 items-center space-x-2 *:flex *:items-center *:gap-2">
+               <div className="pt-5 flex justify-between items-center gap-6 space-x-2 lg:grid lg:grid-cols-2 *:flex *:items-center *:gap-2 *:text-sm md:text:md">
                   <span>
                      <img loading="lazy" src="/home/medal.svg" />
                      <p>989 Skills</p>
@@ -87,7 +92,7 @@ const MarketPlace = () => {
                      <p>45 Sub-Categories</p>
                   </span>
 
-                  <span className="!ml-0 mt-2">
+                  <span className="!ml-0 lg:mt-2">
                      <img loading="lazy" src="/home/badge.svg" />
                      <p>1011 Profiles</p>
                   </span>
@@ -97,14 +102,16 @@ const MarketPlace = () => {
             {/*  */}
             <Card
                id="card"
-               className="max-w-[740px] py-[18px] px-7 rounded-sm shadow-none"
+               className="max-w-[740px] w-full py-[18px] px-4 rounded-sm shadow-none sm:mx-auto lg:px-7"
             >
                <CardHeader className="px-0 py-0">
-                  <CardTitle className="font-medium text-md">
+                  <CardTitle className="font-medium text-sm lg:text-md">
                      Design & Creative
                   </CardTitle>
+               </CardHeader>
 
-                  <CardContent className="pt-2 px-0 pb-0 flex gap-4">
+               <div className="overflow-x-auto">
+               <CardContent className="pt-2 px-0 pb-0 flex gap-4">
                      <div className="w-fit text-center space-y-2">
                         <span className="size-20 flex items-center justify-center bg-[#F6F6F6] rounded-lg">
                            <img
@@ -134,15 +141,15 @@ const MarketPlace = () => {
                         </div>
                      ))}
                   </CardContent>
-               </CardHeader>
+               </div>
             </Card>
          </div>
 
          {/*=== OTHERS ===*/}
-         <div className="flex justify-between">
+         <div className="max-lg:max-w-[750px] w-full place-self-center flex flex-wrap-reverse gap-8 items-center justify-between lg:grid lg:grid-cols-2">
             <div
                id="text-cont"
-               className="max-w-[350px] flex items-center gap-3 text-[22px]"
+               className="max-w-[250px] w-full flex items-center gap-3 text-[22px]"
             >
                <MyButton
                   text="Explore More"
@@ -152,10 +159,11 @@ const MarketPlace = () => {
                />
             </div>
 
-            {/*  */}
+
+            
             <div
                id="card"
-               className="max-w-[740px] w-full items-start rounded-sm shadow-none"
+               className="items-start rounded-sm shadow-none lg:max-w-[750px] lg:w-full"
             >
                <p className="font-medium text-[22px]">
                   <button>
